fba8106008d735d476e89b19455c991b3c32f47c
Adam <developeradamw@gmail.com>
2020-11-10 17:55:57 +0100

    version 0.0

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace polynomial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to program calculating polynomial value!\nThis program uses Homer's algorithm.");
+            Console.WriteLine("f(x) = x*a0 + (x^2)*a1 + ... + (x^n-1)*a(n - 2) + (x^n)*a(n - 1) + an");
+            Console.WriteLine("First input polynomial grade: ");
+            int grade = Convert.ToInt32(Console.ReadLine());
+            int elements = grade + 1;
+            int[] factors = new int[elements];
+            for (int i = 0; i < elements; i++)
+            {
+                Console.WriteLine("Input " + (i+1).ToString() + " factor: ");
+                factors[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Now give an argument: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int result = 0;
+            for (int h = 0; h < grade+1; h++)
+            {
+                double r =  Math.Pow(x, h);
+                result += factors[h] * Convert.ToInt32(r);
+            }
+
+            Console.WriteLine("Your result is " + result.ToString());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+### Polynomial Calculator
+
+<br />
+
+This program was made by me completely from scratch. I did not use any ideas or code from other people.
+Gmail adress to contact with me: developeradamw@gmail.com
+
+### Warning
+<br/>
+The author is not responsible for any damage to files or system. Use at your own risk.
+<br />
+Do not edit any files! If some changes will be done to these files, program stop working properly!
+<br />
+
+### Instructions
+
+<br/>
+You can run program going to bin folder and opening polynomial.exe file.
+<br />
